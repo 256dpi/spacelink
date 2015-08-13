9e1c523c68cf5998ea87c4bad6e956c41143ccc3
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-08-13 16:28:45 +0200

    fix a perspective bug

--- js/components/depth_render.js
+++ js/components/depth_render.js
@@ -23,6 +23,7 @@ function DepthRender(stream, reduce, renderEngine, rotation) {
 
   this.system = new THREE.PointCloud(this.particles, this.material);
   this.system.rotation.y = rotation;
+  this.system.position.y += 120; //TODO: is this a fix value?
   this.system.frustumCulled = false;
 
   this.renderEngine.scene.add(this.system);
